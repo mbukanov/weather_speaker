9d3f4e75cff6dda8086b3dc10d806a345d052245
qeed <dqamex@gmail.com>
2018-06-07 01:09:30 +0300

    added emoton to link to speech class

--- speech_class.py
+++ speech_class.py
@@ -2,20 +2,21 @@
 import urllib
 
 class YandexSpeech(object):
-	def __init__(self, api_key, yformat = "mp3", lang = "ru-RU", speaker = "jane"):
+	def __init__(self, api_key, yformat = "mp3", lang = "ru-RU", speaker = "jane", emotion = "good"):
 		self.api_key = api_key;
 		self.format = yformat;
 		self.lang = lang;
 		self.speaker = speaker;
+		self.emotion = emotion;
 
 	def getLink(self, text):
-		link = "https://tts.voicetech.yandex.net/generate?key="+str(self.api_key)+"&text="+urllib.quote(text)+"&format="+str(self.format)+"&lang="+str(self.lang)+"&speaker="+str(self.speaker);
+		link = "https://tts.voicetech.yandex.net/generate?key="+str(self.api_key)+"&text="+urllib.quote(text)+"&format="+str(self.format)+"&lang="+str(self.lang)+"&speaker="+str(self.speaker)+"&emotion="+str(self.emotion);
 		return link;
 
 	def downloadMp3File(self, link):
 		urllib.urlretrieve(link, str("/tmp/yandex_speech.mp3"));
 		
-	def MakeVoiceFile(self, text):
+	def makeVoiceFile(self, text):
 		link = self.getLink(text);
 		self.downloadMp3File(link);
 
